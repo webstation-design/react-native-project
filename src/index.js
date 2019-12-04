9de0da4c7511ef5d67e4dfea296389aba609e66c
Robson <robson@webstationdesign.com.br>
2019-12-03 22:14:40 -0300

    Ajustes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
