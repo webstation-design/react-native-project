a91fa8b0ef4927d5cd42f950ef2ef4d36f722d31
Robson <robson@webstationdesign.com.br>
2019-12-04 22:10:07 -0300

    React Navigation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import 'react-native-gesture-handler'
 
 import './config/ReactotronConfig';
 
-//console.tron.log('teste');
+import Routes from './routes';
 
 export default function App() {
-  return <View />
+  return <Routes />
 }
