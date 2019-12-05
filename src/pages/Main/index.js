9828083dbaeb292b25c866067522d314ad1f9274
Robson <robson@webstationdesign.com.br>
2019-12-04 23:39:49 -0300

    API GitHub

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Main() {
-  return (
-    <View />
-  );
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+
+  }
+
+  render() {
+
+    const { newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+
 }
 
 Main.navigationOptions = {
