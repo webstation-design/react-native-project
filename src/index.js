3d34be09e2f2064d96ace1bb1dfd2c3b0228262c
Robson <robson@webstationdesign.com.br>
2019-12-04 21:51:24 -0300

    Reactotron

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import './config/ReactotronConfig';
+
+//console.tron.log('teste');
+
+export default function App() {
+  return <View />
+}
